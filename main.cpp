e9429f809a0205308f0190610b0ff118b35fae30
bmileski <67343617+bryanmilesk2020@users.noreply.github.com>
2024-09-23 16:59:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <pcap.h>
 #include <boost/asio.hpp>
-//#include <libnet.h>
+//#include <libnet.h> - will include later
 
 int main() {
-	pcap_if_t* alldevs;
+	pcap_if_t* alldevs; /*sets up linked list of network devices*/
 	pcap_if_t* d;
 	int i = 0;
 	char errbuf[PCAP_ERRBUF_SIZE];
+
+	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1) {
+		frpintf(stderr, "Error in pcap_findalldevs_ex: %s\n", errbuf);
+		exit(1);
+	} /*gathers all available network interfaces for capturing, prints error message if
+	  there is a failure and exits program*/
 }
